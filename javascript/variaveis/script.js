c6425b634f766310a3c7984864469cd545c3c373
Jorge Harbes <jharbes@hotmail.com>
2022-11-08 11:13:58 -0300

    Update script.js

---
+++ javascript/variaveis/script.js
@@ -0,0 +1,11 @@
+var nome='André'
+var idade=28
+var possuiFaculdade=true
+
+console.log(nome,idade,possuiFaculdade)
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+console.log(precoTotal)
