6bfe88b15228a132b18dc28698f779c9dff75cb8
Jorge Harbes <jharbes@hotmail.com>
2022-11-08 14:42:38 -0300

    Create exercicio.js

---
+++ javascript/tipos-de-dados/exercicio.js
@@ -0,0 +1,26 @@
+// Declare uma variável contendo uma string
+
+var variavel1='Fluminense'
+
+// Declare uma variável contendo um número dentro de uma string
+
+var var2='1000 gols'
+
+// Declare uma variável com a sua idade
+
+var idade=39
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var nome='Jorge'
+var sobrenome='Nami Harbes'
+nome+sobrenome
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var pedido="It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(typeof nome)
