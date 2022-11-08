db724fcff0af8604dfe5949e9f2f0c56457f5095
Jorge Harbes <jharbes@hotmail.com>
2022-11-08 16:43:31 -0300

    boolean e condicionais

---
+++ javascript/boolean-e-condicionais/script.js
@@ -0,0 +1,11 @@
+var possuiGraduacao = true;
+var possuiDoutorado = false;
+
+var possuiGraduacao = true;
+
+if(possuiGraduacao) {
+  console.log('Possui graduação');
+} else {
+  console.log('Não possui graduação');
+}
+// retorna Possui Graduação e não executa o else
