775ea44f5350af0b710221dcd21eeef089adee63
Jorge Harbes <jharbes@hotmail.com>
2022-11-08 12:37:53 -0300

    Create exercicio.js

---
+++ javascript/variaveis/exercicio.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+
+var nome='Jorge'
+
+// Declarar uma variável com a sua idade
+
+var idade=39
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaFavorita
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita='churrasco'
+
+// Declarar 5 variáveis diferentes sem valores
+
+var n1,n2,n3,n4,n5
