8a21ccf9b8eedab8fa140f9a9ac31fa7b9fba7f9
Jorge Harbes <jharbes@hotmail.com>
2022-11-08 15:11:20 -0300

    numeros e operadores 2

---
+++ javascript/numeros-e-operadores/script.js
@@ -0,0 +1,27 @@
+// utilizamos a notacao 'e' para representar o expoente 10 elevado a algum numero multiplicado pelo numero antecessor, exemplo:
+
+var num1=2e10,
+    num2=1e5,
+    num3=3e-3
+var idade = 28;
+var gols = 1000;
+var pi = 3.14; // ponto para decimal
+var exp = 2e10; // 20000000000
+    
+console.log(num1,num2,num3)
+
+// operadores aritmeticos basicos
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+// operadores aritmeticos strings
+
+var soma = '100' + 50; // 10050
+var subtracao = '100' - 50; // 50
+var multiplicacao = '100' * '2'; // 200
+var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
+
