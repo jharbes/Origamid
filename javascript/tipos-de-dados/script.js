82a4f60a26c3ff9b0b83e16896d41c4ff9daeedc
Jorge Harbes <jharbes@hotmail.com>
2022-11-08 13:07:28 -0300

    Update script.js

---
+++ javascript/tipos-de-dados/script.js
@@ -0,0 +1,9 @@
+var nome='Andre'
+let idade=28
+let time
+let time2=null
+let simbolo=Symbol()
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof time,typeof time2,typeof simbolo)
