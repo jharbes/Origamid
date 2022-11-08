5b90f938f58ce59c6354e5f6f96d4844ff310550
Jorge Harbes <jharbes@hotmail.com>
2022-11-08 17:32:58 -0300

    Update script.js

--- javascript/boolean-e-condicionais/script.js
+++ javascript/boolean-e-condicionais/script.js
@@ -9,3 +9,24 @@ if(possuiGraduacao) {
   console.log('Não possui graduação');
 }
 // retorna Possui Graduação e não executa o else
+
+
+// operacoes que retornam como falso em javascript:
+// Falsy
+if(false);
+if(0) // ou -0
+if(NaN)
+if(null)
+if(undefined)
+if('') // ou "" ou ``
+
+
+// operacoes que retornam como true em javascript:
+// Truthy
+if(true)
+if(1)
+if(' ')
+if('andre')
+if(-5)
+if({});
+console.log('teste')
