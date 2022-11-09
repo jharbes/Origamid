e027ec6bb305693c7d53db20bbe2130636ee0272
Jorge Harbes <jharbes@hotmail.com>
2022-11-09 11:51:55 -0300

    Update script.js

---
+++ javascript/funcoes/script.js
@@ -0,0 +1,20 @@
+function areaQuadrado(lado) {
+  return lado * lado;
+}
+
+areaQuadrado(4) // 16
+areaQuadrado(5) // 25
+areaQuadrado(2) // 4
+
+console.log(areaQuadrado) // sem os parenteses ele retornara a própria funcao em si
+
+
+// IMPORTANTE***: Diferenca entre parametros e argumentos
+// peso e altura são os parâmetros
+function imc(peso, altura) {
+  const imc = peso / (altura ** 2);
+  return imc;
+}
+
+imc(80, 1.80) // 80 e 1.80 são os argumentos
+imc(60, 1.70) // 60 e 1.70 são os argumentos
