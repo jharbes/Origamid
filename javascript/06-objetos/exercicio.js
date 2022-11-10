4e56b084f212f911b4a40fc7eb62a80da2c1cf94
Jorge Harbes <jharbes@hotmail.com>
2022-11-10 08:44:07 -0300

    Update exercicio.js

---
+++ javascript/06-objetos/exercicio.js
@@ -0,0 +1,33 @@
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+var eu={
+    nome:'Jorge',
+    sobrenome:'Nami Harbes',
+    idade:39,
+    profissao:'Software Developer',
+    altura:1.82,
+    peso:85,
+}
+
+console.table(eu)
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+eu.nomeCompleto=function(){
+    return `${this.nome} ${this.sobrenome}`
+}
+
+console.log(eu.nomeCompleto())
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+  }
+  
+  // Crie um objeto de um cachorro que represente um labrador,
+  // preto com 10 anos, que late ao ver um homem
+  
+  
