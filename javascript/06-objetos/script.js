244dec9ce06c070905f345cc0bea791e760bfaed
Jorge Harbes <jharbes@hotmail.com>
2022-11-10 08:07:49 -0300

    Update script.js

--- javascript/06-objetos/script.js
+++ javascript/06-objetos/script.js
@@ -32,3 +32,30 @@ quadrado.area(5); // 25
 quadrado.perimetro(5); // 20
 
 console.log(quadrado.area(5))
+
+
+// OBJETO COM ABREVIACAO NA SINTAXE DO MÉTODO ABAIXO:
+// IMPORTANTE**** Os métodos também devem ser separados por vírgula
+
+var quadrado2 = {
+  lados: 4,
+  area(lado) {
+    return lado * lado;
+  },
+  perimetro(lado) {
+    return this.lados * lado;
+  },
+}
+
+
+// Objetos servem para organizar o código em pequenas partes reutilizáveis.
+
+// Math é um objeto nativo de JavaScript. Já percebeu que console é um objeto e log() um método?
+
+Math.PI; // 3.14
+Math.random(); // número aleatório
+
+var pi = Math.PI;
+console.log(pi); // 3.14
+
+
