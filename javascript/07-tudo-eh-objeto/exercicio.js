250aeb6f4252bddeb2482a94ef9bd17f9e4d6890
Jorge Harbes <jharbes@hotmail.com>
2022-11-10 10:00:54 -0300

    Update exercicio.js

--- javascript/07-tudo-eh-objeto/exercicio.js
+++ javascript/07-tudo-eh-objeto/exercicio.js
@@ -1,6 +1,25 @@
 // nomeie 3 propriedades ou métodos de strings
 
+var nome='Jorge Nami Harbes'
+
+nome.indexOf('J') // método
+console.log(nome.indexOf('J'))
+
+nome.length // propriedade
+console.log(nome.length)
+
+nome.toLowerCase() // método
+console.log(nome.toLowerCase())
+
+
 // nomeie 5 propriedades ou métodos de elementos do DOM
+
+document.addEventListener
+document.ATTRIBUTE_NODE
+document.getElementsByClassName('red')
+
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+Clipboard
