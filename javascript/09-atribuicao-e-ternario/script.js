ae008df33eccd64891d427ecdc10e3e97c68c6f0
Jorge Harbes <jharbes@hotmail.com>
2022-11-11 14:17:46 -0300

    aula atribuicao e ternario

---
+++ javascript/09-atribuicao-e-ternario/script.js
@@ -0,0 +1,28 @@
+// Comentário de uma linha
+
+/*
+Comentário
+com diversas
+linhas
+*/
+
+// var nome = 'André';
+
+
+console.log('//-----------------------------------------------------------------------------')
+
+// OPERADORES DE ATRIBUIÇÃO
+// Podem funcionar como formas abreviadas
+
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+
+console.log('//-----------------------------------------------------------------------------')
+
