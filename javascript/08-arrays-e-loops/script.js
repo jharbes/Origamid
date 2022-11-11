02b972bc35ec1cfe30656a6ecfdf5f5fb0ec1e5b
Jorge Harbes <jharbes@hotmail.com>
2022-11-11 11:15:03 -0300

    acerto nome diretorio

---
+++ javascript/08-arrays-e-loops/script.js
@@ -0,0 +1,19 @@
+// ARRAY
+// É um grupo de valores geralmente relacionados. Servem para guardarmos diferentes valores em uma única variável.
+
+var videoGames = ['Switch', 'PS4', 'XBox'];
+
+videoGames[0] // Switch
+videoGames[2] // Xbox
+
+
+// MÉTODOS E PROPRIEDADES DE UMA ARRAY
+// Existem diversos outros métodos, como map, reduce, forEach e mais que veremos mais à frente
+
+var videoGames = ['Switch', 'PS4', 'XBox'];
+
+videoGames.pop(); // Remove o último item e retorna ele
+videoGames.push('3DS'); // Adiciona ao final da array
+videoGames.length; // 3 (retorna o tamanho do array ou numero de elementos)
+
+
