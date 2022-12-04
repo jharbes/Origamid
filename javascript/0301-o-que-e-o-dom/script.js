f1758918bf1dd3f9bd15a4bf46b2732836dac00d
Jorge Harbes <jharbes@hotmail.com>
2022-12-04 18:16:08 -0300

    Update script.js

---
+++ javascript/0301-o-que-e-o-dom/script.js
@@ -0,0 +1,10 @@
+console.log(window) // observe no console que ele retorna um objeto, esse objeto possui atributos e metodos como qualquer outro objeto e nada mais eh do que toda a tela disponivel
+
+window.alert('Isso mesmo') // Utiliza o metodo de alerta para mostrar uma mensagem na tela
+
+console.log(window.location) // Mostra todas as informacoes de localizacao (site), mais um atributo (objeto) de window
+
+console.log(window.location.href) // Mostra, por exemplo, o endereco do site (sendo um atributo do objeto location de window)
+
+const href=window.location.href // Nesse caso podemos nomear uma variavel ou constante como um objeto e/ou atributo para melhor manipularmos ele
+
