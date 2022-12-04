437b320cb1913aece05cd9cc35949390a68355b3
Jorge Harbes <jharbes@hotmail.com>
2022-12-04 19:26:14 -0300

    fim aula - o que é o dom

--- javascript/0301-o-que-e-o-dom/exercicio.js
+++ javascript/0301-o-que-e-o-dom/exercicio.js
@@ -5,7 +5,17 @@ console.log(window.location.href)
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
 
-console.log(document.getElementsByClassName['.ativo'])
+console.log(document.getElementsByClassName('ativo')[0])
+
+const primeiroAtivo=document.querySelector('.ativo') // seleciona apenas o primeiro elemento que possui a classe ativo
+
+const todosAtivos=document.querySelectorAll('.ativo') // seleciona todos os elementos que possuem a classe ativo
+
+console.log(primeiroAtivo)
+console.log(todosAtivos) // nesse caso acessaremos os elementos como se fosse um array:
+
+console.log(todosAtivos[0]) // primeiro elemento
+console.log(todosAtivos[1]) // segundo elemento
 
 // Retorne a linguagem do navegador
 
