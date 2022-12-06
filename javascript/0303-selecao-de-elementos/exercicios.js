d0a6426bf8fffe5f7490122e0535cdb39b76716c
Jorge Harbes <jharbes@hotmail.com>
2022-12-06 12:16:58 -0300

    Update exercicios.js

--- javascript/0303-selecao-de-elementos/exercicios.js
+++ javascript/0303-selecao-de-elementos/exercicios.js
@@ -1,10 +1,25 @@
 // Retorne no console todas as imagens do site
 
+console.log(document.getElementsByTagName('img'))
+console.log(document.querySelectorAll('img'))
+
 // Retorne no console apenas as imagens que começaram com a palavra imagem
+
+const imagensImg=document.querySelectorAll([source="img"])
+console.log(imagensImg)
 
 // Selecione todos os links internos (onde o href começa com #)
 
-// Selecione o primeiro h2 dentro de .animais-descricao
+const linksInternos=document.querySelectorAll([href="#"])
+console.log(linksInternos)
+
+// Selecione o primeiro h2 dentro de .dragoes-descricao
+
+const primeiroH2Animais=document.querySelector('.dragoes-descricao h2')
+console.log(primeiroH2Animais)
 
 // Selecione o último p do site
 
+const todosP=document.getElementsByTagName('p')
+const ultimoP=todosP[todosP.length-1]
+console.log(ultimoP)
