dd12eb3583c24da707a066a21a0e7af650b4b455
Jorge Harbes <jharbes@hotmail.com>
2022-12-06 08:55:32 -0300

    iniciando selecao de elementos do dom com javascript

---
+++ javascript/0303-selecao-de-elementos/script.js
@@ -0,0 +1,6 @@
+// Seleciona pelo ID
+const animaisSection = document.getElementById('animais');
+const contatoSection = document.getElementById('contato');
+
+// Retorna null caso não exista
+const naoExiste = document.getElementById('teste');
