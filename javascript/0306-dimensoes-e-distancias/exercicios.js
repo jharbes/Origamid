cd2be5eeb69f443c2de8dca1f1213f16372a488d
Jorge Harbes <jharbes@hotmail.com>
2022-12-07 12:46:30 -0300

    Update exercicios.js

--- javascript/0306-dimensoes-e-distancias/exercicios.js
+++ javascript/0306-dimensoes-e-distancias/exercicios.js
@@ -23,5 +23,17 @@ console.log(somaWidth)
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
 
+
+const todosLinks=document.querySelectorAll('a')
+
+todosLinks.forEach((item,index)=>{
+    if (item.getBoundingClientRect().width>=48 && item.getBoundingClientRect().height>=48)
+        console.log(`Link de numero ${index} de acordo com especificacoes Google`)
+    else
+        console.log(`Link de numero ${index} em desacordo com as especificacoes Google`)
+})
+
+
+
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
