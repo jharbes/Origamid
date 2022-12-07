fa7ebd9a5d9987cdcd4d0da5bef235677d3c835e
Jorge Harbes <jharbes@hotmail.com>
2022-12-07 12:38:51 -0300

    Update exercicios.js

--- javascript/0306-dimensoes-e-distancias/exercicios.js
+++ javascript/0306-dimensoes-e-distancias/exercicios.js
@@ -1,7 +1,23 @@
 // Verifique a distância da primeira imagem
 // em relação ao topo da página
 
+console.log(document.querySelector('img').offsetTop)
+
+
+
 // Retorne a soma da largura de todas as imagens
+
+
+const todasImagens=document.querySelectorAll('img')
+
+let somaWidth=0
+todasImagens.forEach((item,index)=>{
+    somaWidth+=item.getBoundingClientRect().width
+})
+
+console.log(somaWidth)
+
+
 
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
