2361f8d539be84e7539a311c596241306c42fb81
Jorge Harbes <jharbes@hotmail.com>
2023-01-17 05:10:54 -0300

    fim exercicios modulo 0308 minha versao

--- javascript/0308-transversing-e-manipulacao/exercicios.js
+++ javascript/0308-transversing-e-manipulacao/exercicios.js
@@ -1,7 +1,23 @@
 // Duplique o menu e adicione ele em copy
 
+const copy=document.querySelector('.menu').cloneNode(true)
+
 // Selecione o primeiro DT da dl de Faq
+
+const primeiroDt=document.querySelector('.faq-lista').children[0]
+
 
 // Selecione o DD referente ao primeiro DT
 
-// Substitua o conteúdo html de .faq pelo de .animais
+const ddDoPrimeiroDt=primeiroDt.nextElementSibling
+
+
+// Substitua o conteúdo html de .faq pelo de .dragoes
+
+const docFaq=document.querySelector('.faq')
+const tituloFaq=docFaq.querySelector('.titulo')
+const faqLista=docFaq.querySelector('.faq-lista')
+const dragoes=document.querySelector('.dragoes')
+
+docFaq.removeChild(tituloFaq)
+docFaq.replaceChild(dragoes,faqLista)
