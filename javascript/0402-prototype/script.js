e00d4f33fa1bfe9f20be6a0e3c0d96ffe1fecb5f
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-01-25 19:22:20 -0300

    Update script.js

--- javascript/0402-prototype/script.js
+++ javascript/0402-prototype/script.js
@@ -13,3 +13,75 @@ const andre = new Pessoa('André', 28);
 
 console.log(Pessoa.prototype); // retorna o objeto
 console.log(andre.prototype); // undefined
+console.log(typeof Pessoa.prototype)
+
+
+
+/*
+
+FUNCAO.PROTOTYPE
+É possível adicionar novas propriedades e métodos ao objeto prototype.
+
+*/
+
+Pessoa.prototype.andar = function() {
+  return this.nome + ' andou';
+}
+Pessoa.prototype.nadar = function() {
+  return this.nome + ' nadou';
+}
+console.log(Pessoa.prototype); // retorna o objeto
+
+
+
+/*
+
+ACESSO AO PROTÓTIPO
+O objeto criado utilizando o construtor, possui acesso aos métodos e propriedades do protótipo deste construtor. Lembrando, prototype é uma propriedade de funções apenas.
+
+*/
+
+andre.nome;
+console.log(andre.nome)
+andre.idade;
+console.log(andre.idade)
+andre.andar();
+console.log(andre.andar())
+andre.nadar();
+console.log(andre.nadar())
+
+
+
+/*
+
+PROTO
+O novo objeto acessa os métodos e propriedades do protótipo através da propriedade __proto__. É papel da engine fazer essa busca, não devemos falar com __proto__ diretamente.
+
+*/
+
+// Acessam o mesmo método
+// mas __proto__ não terá
+// acesso ao this.nome
+
+andre.andar();
+console.log(andre.andar())
+andre.__proto__.andar();
+console.log(andre.__proto__.andar())
+
+
+
+/*
+
+HERANÇA DE PROTÓTIPO
+O objeto possui acesso aos métodos e propriedades do protótipo do construtor responsável por criar este objeto. O objeto abaixo possui acesso a métodos que nunca definimos, mas são herdados do protótipo de Object.
+
+*/
+
+Object.prototype;
+console.log(Object.prototype)
+andre.toString();
+console.log(andre.toString())
+andre.isPrototypeOf();
+console.log(andre.isPrototypeOf())
+andre.valueOf();
+console.log(andre.valueOf())
