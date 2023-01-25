5d7ef8cce499df8f9008cbb86b98fc644520c21e
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-01-24 23:22:50 -0300

    fim exercicio.js minha resolucao

--- javascript/0401-constructor-functions/exercicio.js
+++ javascript/0401-constructor-functions/exercicio.js
@@ -6,10 +6,26 @@ const pessoa = {
       console.log(this.nome + ' andou');
     }
   }
+
+
+function Pessoa(nome,idade){
+    this.nome=nome
+    this.idade=idade
+    this.andar=function(){
+        console.log(this.nome + ' andou')
+    }
+}
+
+const jorge=new Pessoa('Jorge',39)
+console.log(jorge)
+jorge.andar()
   
   // Crie 3 pessoas, João - 20 anos,
   // Maria - 25 anos, Bruno - 15 anos
-  
+
+const joao=new Pessoa('João',20)
+const maria=new Pessoa('Maria',25)
+const bruno=new Pessoa('Bruno',15)
   
   // Crie uma Constructor Function (Dom) para manipulação
   // de listas de elementos do dom. Deve conter as seguintes
@@ -20,3 +36,27 @@ const pessoa = {
   // removeClass(classe), remove a classe a todos os elementos
   
   
+function DomNovo(elements){
+    this.elements=document.querySelectorAll(elements)
+
+    this.addClass=function(classe){
+        this.elements.forEach((item)=>{
+            item.classList.add(classe)
+        })
+    }
+
+    this.removeClass=function(classe){
+        this.elements.forEach((item)=>{
+            item.classList.remove(classe)
+        })
+    }
+}
+
+const lista5=new DomNovo('li')
+console.log(lista5)
+console.log(lista5.elements)
+console.log(lista5.elements)
+lista5.addClass('novaClasse')
+lista5.addClass('gama')
+lista5.removeClass('gama')
+lista5.removeClass('ativar')
