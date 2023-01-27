0d1e54570557a3f0e024e7d353f8aa051a14d4a1
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-01-26 21:18:32 -0300

    Update exercicio.js

---
+++ javascript/0403-native-host-e-user/exercicio.js
@@ -0,0 +1,30 @@
+// Liste 5 objetos nativos
+
+Object
+Boolean
+String
+Number
+Function
+
+
+
+
+// Liste 5 objetos do browser
+
+Document
+console.log(typeof Document)
+NodeList
+HTMLCollection
+Element
+DOMException
+
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+if (typeof document.webkitHidden !== 'undefined')
+    console.log('existe')
+else
+    console.log('nao existe')
+
+
