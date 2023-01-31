892ba2cb0140a4761372613266646606db662129
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-01-31 18:54:13 -0300

    fim exercicios 0404 minha resolucao

--- javascript/0404-string/exercicio.js
+++ javascript/0404-string/exercicio.js
@@ -36,24 +36,47 @@ transacoes1.forEach((item)=>{
 })
 console.log('Taxa '+somaTaxa)
 console.log('Recebimento '+somaRecebimento)
-  
-  // Retorne uma array com a lista abaixo
-  const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
-  const arrayTransportes=transportes.split(';')
-  console.log(arrayTransportes)
-  
-  // Substitua todos os span's por a's
-  const html = `<ul>
-                  <li><span>Sobre</span></li>
-                  <li><span>Produtos</span></li>
-                  <li><span>Contato</span></li>
-                </ul>`;
-  
-  // Retorne o último caracter da frase
-  const frase5 = 'Melhor do ano!';
-  
-  // Retorne o total de taxas
-  const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
-  
-  
+
+
+
+
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+const arrayTransportes=transportes.split(';')
+console.log(arrayTransportes)
+
+
+
+
+// Substitua todos os span's por a's
+const html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+            </ul>`;
+
+const htmlArray1=html.split('span')
+const htmlJoin=htmlArray1.join('a')
+console.log(htmlJoin)
+
+
+
+
+// Retorne o último caracter da frase
+const frase5 = 'Melhor do ano!';
+const ultimoCaractere=frase5[frase5.length-1]
+console.log(ultimoCaractere)
+
+
+
+
+// Retorne o total de taxas
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+
+let totalTaxas=0
+transacoes2.forEach((item)=>{
+    if (item.toLowerCase().includes('taxa'))
+        totalTaxas+=1
+})
+console.log(totalTaxas)
   
