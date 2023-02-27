a7c375ba8302a80e02e219ff64261f9cf1bd3dfb
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-02-27 17:50:51 -0300

    0405 exercicios minha solucao

--- javascript/0405-number-e-math/exercicio.js
+++ javascript/0405-number-e-math/exercicio.js
@@ -1,10 +1,18 @@
 // Retorne um número aleatório
 // entre 1050 e 2000
 
+console.log(Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050)
+
+
+
 
 // Retorne o maior número da lista abaixo
 const numeros = '4, 5, 20, 8, 9';
-console.log(numeros, typeof numeros)
+
+numerosArray=numeros.split(',')
+console.log(Math.max.apply(null, numerosArray))
+
+
 
 
 // Crie uma função para limpar os preços
@@ -12,3 +20,17 @@ console.log(numeros, typeof numeros)
 // depois retorne a soma total
 const listaPrecos = ['R$ 59,99', ' R$ 100,222',
                      'R$ 230  ', 'r$  200'];
+
+const limpaPrecos=(arrayPrecos)=>{
+    novoArrayPrecos=[]
+    arrayPrecos.forEach((item)=>{
+        item=item.toLowerCase()
+        item=item.replace(',','.').replace('r','').replace('$','').trim()
+        item=+item
+        item=+item.toFixed(2)
+        novoArrayPrecos.push(item)
+    })
+    return novoArrayPrecos
+}
+console.log(limpaPrecos(listaPrecos))
+console.log(listaPrecos)
