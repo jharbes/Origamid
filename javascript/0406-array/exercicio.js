9c7d139019f71dcb311a2611e073d5b65c3a935b
Jorge Harbes <jharbes@hotmail.com>
2023-03-01 07:25:10 -0300

    0406 array exercicios minha resolucao feita

--- javascript/0406-array/exercicio.js
+++ javascript/0406-array/exercicio.js
@@ -21,9 +21,24 @@ console.log("file: exercicio.js:17 ~ comidas", comidas)
 
 const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Arrume os estudantes em ordem alfabética
+estudantes.sort()
+console.log("file: exercicio.js:25 ~ estudantes", estudantes)
+
+
 // Inverta a ordem dos estudantes
+estudantes.reverse()
+console.log("file: exercicio.js:30 ~ estudantes", estudantes)
+
+
 // Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes('Joana'))
+
+
 // Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes('Juliana'))
+
+
+
 
 let html = `<section>
               <div>Sobre</div>
@@ -32,7 +47,16 @@ let html = `<section>
             </section>`
 // Substitua section por ul e div com li,
 // utilizando split e join
+html=html.split('section').join('ul').split('div').join('li')
+console.log("file: exercicio.js:51 ~ html", html)
+
+
+
 
 const carros2 = ['Ford', 'Fiat', 'VW', 'Honda'];
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
+const cloneCarros=carros2.slice()
+carros2.pop()
+console.log("file: exercicio.js:60 ~ cloneCarros", cloneCarros)
+console.log("file: exercicio.js:62 ~ carros2", carros2)
